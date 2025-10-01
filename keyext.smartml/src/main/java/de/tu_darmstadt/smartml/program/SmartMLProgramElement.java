83b51cb83d8073f55be6368a80c3072588a023dc
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-10-01 10:58:38 +0200

    Minor extension to SmartMLProgramelement

--- keyext.smartml/src/main/java/de/tu_darmstadt/smartml/program/SmartMLProgramElement.java
+++ keyext.smartml/src/main/java/de/tu_darmstadt/smartml/program/SmartMLProgramElement.java
@@ -2,5 +2,27 @@ package de.tu_darmstadt.smartml.program;
 
 import org.key_project.logic.SyntaxElement;
 
-public abstract class SmartMLProgramElement implements SyntaxElement {
+/// interface at the root of any program element representing
+/// SmartML constructs
+public interface SmartMLProgramElement extends SyntaxElement {
+
+    /// to be populated by methods for matching etc.
+
+    default int computeHashCode() {
+        // Cache for hashcode computation would be of advantage as it is for instance recomputed
+        // for each modality creation and can be rather expensive as the whole AST is repeatedly traversed
+        // at the moment this has to be cached at each subclass
+        /*
+         * if (hashcode != -1) {
+         * return hashcode;
+         * }
+         */
+        int hash = 7;
+        for (int i = 0; i < this.getChildCount(); i++) {
+            hash = hash * 31 + this.getChild(i).hashCode();
+        }
+        // this.hashcode = hash;
+        return hash;
+    }
+
 }
