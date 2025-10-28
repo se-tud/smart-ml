7e133f0b1bf19da62909c5544bbeb64480ab4b1a
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-10-28 14:45:39 +0100

    Added a few tests for parsing formulas

--- keyext.smartml/src/test/java/de/tu_darmstadt/smartml/parser/ParsingFacadeTest.java
+++ keyext.smartml/src/test/java/de/tu_darmstadt/smartml/parser/ParsingFacadeTest.java
@@ -8,11 +8,15 @@ import java.util.HashMap;
 import org.key_project.logic.Name;
 import org.key_project.logic.Term;
 import org.key_project.logic.op.Function;
+import org.key_project.logic.sort.Sort;
 import org.key_project.util.collection.ImmutableArray;
 
 import de.tu_darmstadt.smartml.logic.SmartMLDLTheory;
 import de.tu_darmstadt.smartml.logic.op.Junctor;
+import de.tu_darmstadt.smartml.logic.op.LogicVariable;
+import de.tu_darmstadt.smartml.logic.op.Quantifier;
 import de.tu_darmstadt.smartml.logic.op.SFunction;
+import de.tu_darmstadt.smartml.logic.sort.SortImpl;
 import de.tu_darmstadt.smartml.services.Services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,23 +32,35 @@ class ParsingFacadeTest {
     @BeforeEach
     void setup() {
         services = new Services();
+
+        SortImpl mySort = new SortImpl(new Name("MySort"));
+        services.getNamespaces().sorts().addSafely(mySort);
+
         predicates = new HashMap<>();
         final Function[] atoms = { delcareAtom("A"), delcareAtom("B"),
-            delcareAtom("C"), delcareAtom("D") };
+            delcareAtom("C"), delcareAtom("D"), delcareAtom("p", mySort),
+            delcareAtom("q", mySort, mySort) };
 
         for (Function atom : atoms) {
             predicates.put(atom.name().toString(), atom);
         }
 
         services.getNamespaces().functions().addSafely(predicates.values());
+
+    }
+
+    private Function delcareAtom(String name, Sort... argumentSorts) {
+        return new SFunction(new Name(name), SmartMLDLTheory.FORMULA,
+            new ImmutableArray<>(argumentSorts), true);
     }
 
     private SFunction delcareAtom(String name) {
         return new SFunction(new Name(name), SmartMLDLTheory.FORMULA, new ImmutableArray<>(), true);
     }
 
+
     @Test
-    void parseExpression() {
+    void parsePropositionalFormula() {
         KeYIO io = new KeYIO(services);
         final Term term = io.parseExpression("A & (!B -> (C | D))");
         assertSame(Junctor.AND, term.op());
@@ -56,4 +72,39 @@ class ParsingFacadeTest {
         assertSame(predicates.get("C"), term.sub(1).sub(1).sub(0).op());
         assertSame(predicates.get("D"), term.sub(1).sub(1).sub(1).op());
     }
+
+
+    @Test
+    void parseQuantifiedFormula() {
+        KeYIO io = new KeYIO(services);
+        final Term term = io.parseExpression("\\forall MySort x; p(x)");
+        assertSame(Quantifier.ALL, term.op());
+        assertSame(predicates.get("p"), term.sub(0).op());
+    }
+
+    @Test
+    void parseNestedQuantifiedFormula() {
+        KeYIO io = new KeYIO(services);
+        final Term term = io.parseExpression("\\forall MySort x;\\forall MySort y; q(x, y)");
+        assertSame(Quantifier.ALL, term.op());
+        assertSame(Quantifier.ALL, term.sub(0).op());
+        assertSame(predicates.get("q"), term.sub(0).sub(0).op());
+        assertInstanceOf(LogicVariable.class, term.sub(0).sub(0).sub(0).op());
+        assertEquals(2, ((LogicVariable) term.sub(0).sub(0).sub(0).op()).getIndex());
+        assertEquals(1, ((LogicVariable) term.sub(0).sub(0).sub(1).op()).getIndex());
+    }
+
+
+    @Test
+    void parseNestedQuantifiedFormula2() {
+        KeYIO io = new KeYIO(services);
+        final Term term = io.parseExpression("\\forall MySort y;\\forall MySort x; q(x, y)");
+        assertSame(Quantifier.ALL, term.op());
+        assertSame(Quantifier.ALL, term.sub(0).op());
+        assertSame(predicates.get("q"), term.sub(0).sub(0).op());
+        assertInstanceOf(LogicVariable.class, term.sub(0).sub(0).sub(0).op());
+        assertEquals(1, ((LogicVariable) term.sub(0).sub(0).sub(0).op()).getIndex());
+        assertEquals(2, ((LogicVariable) term.sub(0).sub(0).sub(1).op()).getIndex());
+    }
+
 }
